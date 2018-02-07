6b9a74cee545286d525f6a13442c2d6c5ebe7614
James J <jjackson360@gmail.com>
2018-02-07 14:36:28 -0500

    initial commit

---
+++ TestGit/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ourNumber = 5;
+
+            Console.WriteLine(ourNumber);
+        }
+    }
+}
